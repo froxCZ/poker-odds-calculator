58ff626f86b52dedbbe014ddcaf6348deaef0f4f
Vojtech Udrzal <udrzalv@union.edu>
2014-05-28 22:13:55 -0400

    refactoring

--- InteractiveController.cpp
+++ InteractiveController.cpp
@@ -23,49 +23,10 @@ InterativeController::~InterativeController() {
 void InterativeController::Run() {
     stringstream ss;
     string cmd;
-    int cardCounter=0;
-    while (true) {
-        cin >> cmd;
-        if(cmd=="add"){
-            if(cardCounter==0)cmd="flop";
-            if(cardCounter==3)cmd="turn";
-            if(cardCounter==4)cmd="river";
-        }
-        if (cmd == "new") {
-            Reset();
-            cardCounter = 0;
-            cout << "players: ";
-            cin >> players;
-            game = new Game(players);
-            cout << "card1 and 2: ";
-            cin >> my1;
-            cin >> my2;
-            Simulate();
-        } else if (cmd == "flop") {
-            cout << "flop card1,2,3: ";
-            cin >> f1;
-            cin >> f2;
-            cin >> f3;
-            Simulate();
-            cardCounter+=3;
-        } else if (cmd == "turn") {
-            cout << "turn card4: ";
-            cin >> f4;
-            Simulate();
-            cardCounter++;
-        } else if (cmd == "river") {
-            cout << "river card5: ";
-            cin >> f5;
-            Simulate();
-            cardCounter++;
-        } else if (cmd == "p") {
-            cout << "enter players number: ";
-            cin >> players;
-            delete game;
-            game = new Game(players);
-        }else if(cmd == "sml"){
-            Simulate();
-        }
+    run = true;
+    while (run) {
+        LoadCmd(cmd);
+        ExecuteCmd(cmd);
     }
 }
 
@@ -95,5 +56,62 @@ void InterativeController::Simulate() {
     }
     double chanceToWin = (iWon * 1.0) / gamesToPlay;
     cout << "chance to win: " << chanceToWin << " avg chance: " << 1.0 / players << endl;
+}
+
+void InterativeController::LoadCmd(string & cmd) {
+    cin >> cmd;
+    if (cmd == "add") {
+        if (cardCounter == 0)cmd = "flop";
+        if (cardCounter == 3)cmd = "turn";
+        if (cardCounter == 4)cmd = "river";
+    }
+}
+
+void InterativeController::ExecuteCmd(string cmd) {
+    if (cmd == "new") {
+        Reset();
+        cardCounter = 0;
+        cout << "players: ";
+        cin >> players;
+        game = new Game(players);
+        cout << "card1 and 2: ";
+        cin >> my1;
+        cin >> my2;
+        Simulate();
+    } else if (cmd == "flop") {
+        cout << "flop card1,2,3: ";
+        cin >> f1;
+        cin >> f2;
+        cin >> f3;        
+        cardCounter += 3;
+        Simulate();
+    } else if (cmd == "turn") {
+        cout << "turn card4: ";
+        cin >> f4;        
+        cardCounter++;
+        Simulate();
+    } else if (cmd == "river") {
+        cout << "river card5: ";
+        cin >> f5;
+        cardCounter++;
+        Simulate();
+    } else if (cmd == "p") {
+        cout << "enter players number: ";
+        cin >> players;
+        delete game;
+        game = new Game(players);
+    } else if (cmd == "smlt") {
+        Simulate();
+    } else if (cmd == "exit") {
+        Finish();
+    } else if (cmd == "help") {
+        cout << "print help" << endl;
+    } else {
+        cout << "unknown command: " << cmd << endl;
+    }
+}
+
+void InterativeController::Finish() {
+    run = false;
 }
 
--- InteractiveController.h
+++ InteractiveController.h
@@ -13,14 +13,19 @@ public:
     Game* game;
     string my1,my2;
     string f1,f2,f3,f4,f5;
+    bool run;
     int players;
+    int cardCounter = 0;
     InterativeController();
     InterativeController(const InterativeController& orig);
     virtual ~InterativeController();
     void Run();    
+    void Finish();
 private:
     void Reset();
     void Simulate();
+    void LoadCmd(string&cmd);
+    void ExecuteCmd(string cmd);
 
 };
 
